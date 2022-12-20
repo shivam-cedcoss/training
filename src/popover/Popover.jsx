123ba7c94c32ab90ac1c30f5ef9b5a6a545e2754
shivam-cedcoss <shivamyadav@cedcoss.com>
2022-12-20 19:23:47 +0530

    More Functionality like top,right,bottom,left

--- src/popover/Popover.jsx
+++ src/popover/Popover.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
 import { createPortal } from "react-dom";
 import "./Popover.css";
-const Popover = ({ data, referring }) => {
+
+const Popover = ({ data, referring, position }) => {
   const [flag, setFlag] = useState(false);
   const btnRef = useRef(null);
   const popoverRef = useRef(null);
   const checkVisibility = () => {
     intialPosition();
-    // setFlag(!flag);
     if (!flag) {
       setFlag(true);
     } else {
@@ -15,48 +15,134 @@ const Popover = ({ data, referring }) => {
     }
   };
 
-  if (flag) {
-    window.addEventListener("click", function (e) {
-      if (!popoverRef.current.contains(e.target)) {
-        // setFlag();
-      }
-    });
-  }
-
   const theme = {
     display: flag ? "block" : "none",
-    width: "100px",
-    minHeight: "100px",
-    backgroundColor: "white",
-    boxShadow:
-      "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
-    float: "right",
-    right: "85px",
-    position: "fixed",
-    zIndex: "99",
   };
 
+  const Modal = createPortal(
+    <div style={theme} ref={popoverRef} className="popover__container-popover">
+      {data}
+    </div>,
+    document.body
+  );
+
+  //Detecting window resize
+  const getResizePosition = () => {
+    window.addEventListener("resize", resizeWindow);
+    let dimension = btnRef.current.getBoundingClientRect();
+    let popoverDimension = popoverRef.current.getBoundingClientRect();
+    let w = document.documentElement.clientWidth;
+    let h = document.documentElement.clientHeight;
+    if (h - dimension.bottom > popoverDimension.height) {
+      popoverRef.current.style.top = `${dimension.bottom}px`;
+      popoverRef.current.style.left = `${
+        dimension.left - popoverDimension.width
+      }px`;
+    } else if (h - dimension.bottom < popoverDimension.height) {
+      popoverRef.current.style.top = `${
+        dimension.top - popoverDimension.height
+      }px`;
+      popoverRef.current.style.left = `${
+        dimension.left - popoverDimension.width
+      }px`;
+    }
+  };
+
+  const resizeWindow = () => {
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+    let dimension = btnRef.current.getBoundingClientRect();
+    let popoverDimension = popoverRef.current.getBoundingClientRect();
+    if (h - dimension.bottom < popoverDimension.height) {
+      popoverRef.current.style.top = `${
+        dimension.top - popoverDimension.height
+      }px`;
+      popoverRef.current.style.left = `${
+        dimension.left - popoverDimension.width
+      }px`;
+    } else if (
+      h - dimension.bottom > popoverDimension.height ||
+      w - dimension.right < popoverDimension.width
+    ) {
+      popoverRef.current.style.top = `${dimension.bottom}px`;
+      popoverRef.current.style.left = `${
+        dimension.left - popoverDimension.width
+      }px`;
+    } else if (w - dimension.right > popoverDimension.width) {
+      popoverRef.current.style.top = `${
+        dimension.top - popoverDimension.height
+      }px`;
+      popoverRef.current.style.left = `${dimension.right}px`;
+    }
+  };
+
+  // Initial position
   const intialPosition = () => {
     let dimension = btnRef.current.getBoundingClientRect();
-    console.log(dimension);
-    popoverRef.current.style.top = `${dimension.bottom}px`;
-    popoverRef.current.style.right = `${dimension.x / dimension.left + 85}px`;
+    if (popoverRef.current !== null) {
+      if (position === "default") {
+        popoverRef.current.style.top = `${dimension.bottom}px`;
+        popoverRef.current.style.left = `${dimension.left}px`;
+      } else if (position === "Bottom-left") {
+        popoverRef.current.style.top = `${dimension.bottom}px`;
+        popoverRef.current.style.left = `${dimension.left}px`;
+      } else if (position === "Bottom-right") {
+        popoverRef.current.style.top = `${dimension.bottom}px`;
+        popoverRef.current.style.left = `${dimension.left + dimension.width}px`;
+      } else if (position === "Top-left") {
+        var popoverDimension = popoverRef.current.getBoundingClientRect();
+        popoverDimension = popoverRef.current.getBoundingClientRect();
+        popoverRef.current.style.top = `${
+          dimension.top - popoverDimension.height
+        }px`;
+        popoverRef.current.style.left = `${dimension.left}px`;
+      } else if (position === "Top-right") {
+        popoverDimension = popoverRef.current.getBoundingClientRect();
+        popoverRef.current.style.top = `${
+          dimension.top - popoverDimension.height
+        }px`;
+        popoverRef.current.style.left = `${dimension.left + dimension.width}px`;
+      } else if (position === "Auto") {
+        getResizePosition();
+      }
+    }
   };
 
+  // Gets calls when clicking outside
+  const handleClickOutside = (event) => {
+    if (btnRef.current && !btnRef.current.contains(event.target)) {
+      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+        setFlag(false);
+      }
+    }
+  };
+
+  // Getting position on scrolling
   useEffect(() => {
+    intialPosition();
     if (referring !== undefined) {
       referring.onscroll = intialPosition;
     }
   }, [flag]);
 
+  // Adding the event listener
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
   return (
     <div className="popover__container">
-      <button onClick={checkVisibility} ref={btnRef}>
+      <button
+        onClick={checkVisibility}
+        ref={btnRef}
+        className="popover__container-btn"
+      >
         View Title
       </button>
-      <div style={theme} ref={popoverRef}>
-        {data}
-      </div>
+      {flag && Modal}
     </div>
   );
 };
--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -3,6 +3,7 @@ import Popover from "../popover/Popover";
 import "./Table.css";
 const Table = () => {
   const [post, setPost] = useState([]);
+  const [popoverPosition, setPopoverPosition] = useState("default");
   const divRef = useRef();
   const fetchPost = async () => {
     let response = await fetch(
@@ -15,24 +16,78 @@ const Table = () => {
   useEffect(() => {
     fetchPost();
   }, []);
+
+  // Setting popover position
+  const callPopoverPosition = (e) => {
+    setPopoverPosition(e.target.innerText);
+  };
   return (
-    <div className="container" ref={divRef}>
-      <table>
-        {post.map((i) => {
-          return (
-            <tbody className="container__album" key={i.id}>
-              <tr>
-                <td>{i.id}</td>
-                <td>{i.title}</td>
-                <td>
-                  <Popover data={i.title} referring={divRef.current} />
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
-    </div>
+    <>
+      <div className="container" ref={divRef}>
+        <table>
+          <tbody>
+            {post.map((i, index) => {
+              return (
+                <tr className="container__album" key={i.id}>
+                  <td>{i.id}</td>
+                  <td>{i.title}</td>
+                  <td>
+                    <Popover
+                      data={i.title}
+                      referring={divRef.current}
+                      position={popoverPosition}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="container-btn">
+        <button
+          onClick={callPopoverPosition}
+          style={{
+            backgroundColor: popoverPosition === "Bottom-left" ? "#5fdba7" : "",
+          }}
+        >
+          Bottom-left
+        </button>
+        <button
+          onClick={callPopoverPosition}
+          style={{
+            backgroundColor:
+              popoverPosition === "Bottom-right" ? "#5fdba7" : "",
+          }}
+        >
+          Bottom-right
+        </button>
+        <button
+          onClick={callPopoverPosition}
+          style={{
+            backgroundColor: popoverPosition === "Top-left" ? "#5fdba7" : "",
+          }}
+        >
+          Top-left
+        </button>
+        <button
+          onClick={callPopoverPosition}
+          style={{
+            backgroundColor: popoverPosition === "Top-right" ? "#5fdba7" : "",
+          }}
+        >
+          Top-right
+        </button>
+        <button
+          onClick={callPopoverPosition}
+          style={{
+            backgroundColor: popoverPosition === "Auto" ? "#5fdba7" : "",
+          }}
+        >
+          Auto
+        </button>
+      </div>
+    </>
   );
 };
 
