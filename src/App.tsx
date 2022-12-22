2fc6fa16827fc256e86a3eefefb5ae6ac9cc9529
shivam-cedcoss <shivamyadav@cedcoss.com>
2022-12-22 19:30:55 +0530

    select-component commit-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 import Select from './select/Select';
 function App() {
+  const options = [
+    { value: "1", label: "Russia"},
+    { value: "2", label: "Russia Federation", description: "Lorem ipsum dolor sit amet, consectetur" },
+    { value: "3", label: "Republic of Russia", description: "Lorem ipsum dolor sit amet, consectetur" },
+    { value: "4", label: "Label 4", description: "Lorem ipsum dolor sit amet, consectetur" }
+  ]
   return (
     <>
-      <Select label="Label" searchHelpText={true} />
+      <Select label={null} searchHelpText={false} isSearchable={true} options={options} loading={false} withDescription={true} />
     </>
   );
 }
--- src/select/Select.tsx
+++ src/select/Select.tsx
@@ -1,28 +1,113 @@
-import React, { useState } from 'react';
-import { ChevronDown, Search, XCircle } from 'react-feather';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { ChevronDown } from 'react-feather';
 import './Select.css';
+import '../assests/css/globalCss/style.css';
+import { Svgs } from '../assests/icons/Svgs';
 
 interface ISelect {
   label?: string | null
   isSearchable?: boolean
   searchHelpText?: boolean
+  options?: Array<ObjI>
+  loading?: boolean
+  withDescription?: boolean
 }
 
-const Select = ({ label, searchHelpText, isSearchable }: ISelect) => {
-  const [searchFlag, setsearchFlag] = useState<boolean>(false)
+interface ObjI extends GroupI {
+  label: string | React.ReactNode;
+  value: string;
+  description?: string
+}
+
+interface Itype {
+  value?: string | null,
+  label?: string | null,
+  description?: string | null,
+}
+
+interface GroupI {
+  group?: ObjI[];
+}
+
+const Select = ({ label, searchHelpText, isSearchable, options, loading, withDescription }: ISelect) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selecttext, setSelectText] = useState<string | null>(null);
+  const [matchedOptions, setMatchedOptions] = useState<ObjI[]>([])
+  const getSelectedOption = (e: any) => {
+    setSelectText(e)
+  }
+  const searchFromDropdown = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectText(e.target.value);
+  }
+
+  useEffect(() => {
+    let a = selecttext?.toLowerCase();
+    // setItems([...items, newData])
+    options?.map((i, index) => {
+      // const newData = {
+      //   value: i.value,
+      //   label: i.label,
+      //   description: i.description
+      // }
+      if (typeof i.label === "string" && a !== undefined) {
+        if (i.label.toLowerCase()?.includes(a)) {
+          console.log(i)
+          setMatchedOptions([i])
+        }
+      }
+    })
+    // options?.map((item, index) => {
+    //   if (typeof item.label === "string") {
+    //     let b = item.label.toLowerCase();
+    //     if (b?.substring(0, index) === e.target.value) {
+    //       console.log(item)
+    //     }
+    //   }
+    // });
+
+  }, [selecttext])
+  console.log(matchedOptions.length)
   return (
     <>
       <div className="inte__container">
-        {label && <label className="pb-8">{label}</label>}
-        <div className="inte__container-select">
-          <div>{isSearchable ? <input type="text" placeholder="Select" onChange={() => setsearchFlag(!searchFlag)} /> : "Select"}</div>
+        {label && <label className="pb-8 select-label" htmlFor="select-input">{label}</label>}
+        <div className={showDropdown ? "inte__container-select inte__container-select-focus" : "inte__container-select"} onClick={() => setShowDropdown(!showDropdown)}>
+          <div>{isSearchable ? <input type="text" id="select-input" placeholder="Select" onChange={(e) => searchFromDropdown(e)} value={selecttext !== null ? selecttext : ""} /> : "Select"}</div>
           <div className="inte__container-select-icons">
-            <button style={{display: searchFlag ? "inline-block":"none"}} onClick={() => setsearchFlag(!searchFlag)}><XCircle /></button>
-             <span></span>
-              <ChevronDown />
-             </div>
+            {loading ? <span style={{ border: "none" }} className="inte__container-select-loader">{Svgs.spinnerIcon}</span> : <button style={{ display: selecttext !== null ? "inline-block" : "none" }} onClick={() => setSelectText(null)}>
+              {Svgs.CrossIcon}
+            </button>}
+
+            <span></span>
+            <ChevronDown className={showDropdown ? "icon-up" : "icon-down"} />
+          </div>
         </div>
-        <div className="inte__container-search-text pt-8 ft-16" style={{ display: searchHelpText ? "block" : "none" }}><span><Search size={16} /></span><span className="pl-8">Please enter the text</span></div>
+        {showDropdown && <div className="inte__container-dropdown">
+          {matchedOptions.length === 0 ? options?.map((item, index) => {
+            return (
+              <div key={index} className={selecttext === item.label ? "inte__container-dropdown-items inte__container-dropdown-active" : "inte__container-dropdown-items"} onClick={() => getSelectedOption(item.label)}>
+                <div className={selecttext === item.label ? "" : "inte__container-dropdown-items-hidden"}>{Svgs.tickIcon}</div>
+                <div className="inte__container-dropdown-items-content">
+                  <div>{item.label}</div>
+                  {withDescription && <div>{item.description}</div>}
+                </div>
+              </div>
+            )
+          }) : matchedOptions?.map((item, index) => {
+            return (
+              <div key={index} className={selecttext === item.label ? "inte__container-dropdown-items inte__container-dropdown-active" : "inte__container-dropdown-items"} onClick={() => getSelectedOption(item.label)}>
+                <div className={selecttext === item.label ? "" : "inte__container-dropdown-items-hidden"}>{Svgs.tickIcon}</div>
+                <div className="inte__container-dropdown-items-content">
+                  <div>{item.label}</div>
+                  {withDescription && <div>{item.description}</div>}
+                </div>
+              </div>
+            )
+          })}
+        </div>}
+
+        {searchHelpText && <div className="inte__container-search-text pt-8 ft-16"><span>{Svgs.SearchIcon}
+        </span><span className="pl-8">Please enter the text</span></div>}
       </div>
     </>
   )
